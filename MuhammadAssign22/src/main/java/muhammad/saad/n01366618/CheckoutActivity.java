e0a6aafac12ad937cd175302587110c4f172a77e
MuhammadSaad6618 <muhammadsaad.1274@gmail.com>
2022-02-13 21:55:57 -0500

    Added french language and Did the checkoutScreen

--- MuhammadAssign22/src/main/java/muhammad/saad/n01366618/CheckoutActivity.java
+++ MuhammadAssign22/src/main/java/muhammad/saad/n01366618/CheckoutActivity.java
@@ -2,17 +2,13 @@ package muhammad.saad.n01366618;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class PaymentActivity extends AppCompatActivity {
+public class CheckoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment);
-        Intent act = new Intent(this, CheckoutActivity.class);
-        startActivity(act);
-
+        setContentView(R.layout.activity_checkout);
     }
 }
